c1d3f656ad0c0b168f6e52201a6afd572e7580c3
cy.andersen <cy.andersen@192.168.0.168>
2020-11-04 16:09:57 -0500

    Bonjour

--- ImpotCybèle/src/ImpotCybèleAvecSeulmentLesPourcentage.java
+++ ImpotCybèle/src/ImpotCybèleAvecSeulmentLesPourcentage.java
@@ -29,7 +29,7 @@ public class ImpotCybèleAvecSeulmentLesPourcentage
 		 *Caluler Impôt
 		 *Afficher
 		 *============
-		 *
+		 *Bonjour!
 		 */
 		Scanner sc=new Scanner(System.in);
 		//Déclaration des variables
